b865acd9f8024a524cddd0f2351e4365ebab1897
Andy Ma <philoround@gmail.com>
2021-08-10 17:51:04 -0400

    Update annotation data for new 2000 images w/o politics

--- config_example.py
+++ config_example.py
@@ -1,6 +1,6 @@
 # First use: please duplicate this file and rename it to `config.py`
 # set the csv file path here; only change Name to your name
-csv_path = 'csv_3000_hate/sample_1000_Name.csv'
+csv_path = 'csv_2000_no_politics/sample_650_Name.csv'
 # start index of the labeling; The index is started from 1
 start_idx = 1
 # set end_idx to 0 if you want to label the whole dataset
